158a289c73ef4617bf32445c01e6c93cdfc1715f
naleont <naleont@gmail.com>
2025-03-28 01:17:07 +0300

    weeeeee

--- app.py
+++ app.py
@@ -5972,6 +5972,8 @@ def id_payments(mode, length, page):
 @app.route('/set_payee/<payment_id>', defaults={'payee': None})
 @app.route('/set_payee/<payment_id>/<payee>')
 def set_payee(payment_id, payee):
+    if isinstance(payee, str) and '%' in payee:
+        payee = unquote(payee)
     access = check_access(8)
     if access is not True:
         return access
