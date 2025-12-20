9df7abba57aadb560d4e8f1533d2812da326c510
naleont <naleont@gmail.com>
2025-12-20 12:47:23 +0300

    2026

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ from urllib.parse import unquote
 
 app = Flask(__name__, instance_relative_config=False)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team_db.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team_db_arch_2025.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team_db_arch_2024.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///team_db_arch.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -55,7 +56,8 @@ app.config['MAIL_USE_SSL'] = mail_data.mail['MAIL_USE_SSL']
 mail = Mail(app)
 
 tel_unneeded = '-() '
-curr_year = 2025
+curr_year = 2026
+# curr_year = 2025
 # curr_year = 2024
 # curr_year = 2023
 fee = 5500
